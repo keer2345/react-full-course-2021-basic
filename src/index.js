a8d7c05b3737fed3f60c988e27d62c633411c8f0
keer2345 <keer2345@gmail.com>
2021-06-03 21:44:37 +0800

    props

--- src/index.js
+++ src/index.js
@@ -4,21 +4,33 @@ import ReactDom from 'react-dom'
 // CSS
 import './index.css'
 
-const author = 'Amelia Hepworth'
-const title = 'I Love You to the Moon and Back'
-const img =
-  'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg'
+const firstBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
+  title: 'I Love You to the Moon and Back',
+  author: 'Amelia Hepworth'
+}
+const secondBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg',
+  title: 'Our Class is a Family',
+  author: 'Shannon Olsen'
+}
 
 const BookList = () => (
   <section className='booklist'>
-    <Book />
-    <Book />
-    <Book />
-    <Book />
+    <Book
+      img={firstBook.img}
+      title={firstBook.title}
+      author={firstBook.author}
+    />
+    <Book
+      img={secondBook.img}
+      title={secondBook.title}
+      author={secondBook.author}
+    />
   </section>
 )
 
-const Book = () => {
+const Book = ({ img, title, author }) => {
   return (
     <article className='book'>
       <img src={img} alt='' />
