7e89a1003c1815f586e261aa966486902d3a5b68
keer2345 <keer2345@gmail.com>
2021-06-03 22:35:14 +0800

    key prop & spread operator

--- src/index.js
+++ src/index.js
@@ -6,16 +6,19 @@ import './index.css'
 
 const books = [
   {
+    id: 1,
     img: 'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
     title: 'I Love You to the Moon and Back',
     author: 'Amelia Hepworth'
   },
   {
+    id: 2,
     img: 'https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg',
     title: 'Our Class is a Family',
     author: 'Shannon Olsen'
   },
   {
+    id: 3,
     img: 'https://images-na.ssl-images-amazon.com/images/I/71e5m7xQd0L._AC_UL200_SR200,200_.jpg',
     title: 'The Vanishing Half: A Novel',
     author: 'Brit Bennett'
@@ -25,14 +28,12 @@ const books = [
 const BookList = () => (
   <section className='booklist'>
     {books.map((book) => {
-      return <Book book={book} />
+      return <Book key={book.id} {...book} />
     })}
   </section>
 )
 
-const Book = (props) => {
-  const { img, title, author } = props.book
-
+const Book = ({ img, title, author }) => {
   return (
     <article className='book'>
       <img src={img} alt='' />
