c49228ee593d3c0f91668db19416a4adf454aa8e
keer2345 <keer2345@gmail.com>
2021-06-03 21:18:40 +0800

    JSX JavaScript

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import ReactDom from 'react-dom'
 // CSS
 import './index.css'
 
+const author = 'Amelia Hepworth'
+const title = 'I Love You to the Moon and Back'
+const img =
+  'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg'
+
 const BookList = () => (
   <section className='booklist'>
-    <Book />
-    <Book />
-    <Book />
-    <Book />
     <Book />
     <Book />
     <Book />
@@ -17,26 +18,14 @@ const BookList = () => (
   </section>
 )
 
-const Book = () => (
-  <article className='book'>
-    <Image />
-    <Title />
-    <Author />
-  </article>
-)
-
-const Image = () => (
-  <img
-    src='https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg'
-    alt=''
-  />
-)
-
-const Title = () => <h1>I Love You to the Moon and Back</h1>
-const Author = () => (
-  <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem' }}>
-    Amelia Hepworth
-  </h4>
-)
+const Book = () => {
+  return (
+    <article className='book'>
+      <img src={img} alt='' />
+      <h1>{title}</h1>
+      <h4>{author.toUpperCase()}</h4>
+    </article>
+  )
+}
 
 ReactDom.render(<BookList />, document.getElementById('root'))
